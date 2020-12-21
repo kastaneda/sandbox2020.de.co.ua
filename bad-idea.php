7e080a114f669cd87234ae7ae95e3f391eed5092
Dmitry Kolesnikov <kastaneda@gmail.com>
2020-12-21 20:31:55 +0200

    update

--- bad-idea.php
+++ bad-idea.php
@@ -11,5 +11,6 @@ $url = 'https://sandbox2020.de.co.ua/bad-idea.php';
 
 header('Content-type: text/plain');
 echo "# How to run:\n# curl $url | bash -\n# wget $url -O - | bash -\n\n";
+echo "# Source: https://github.com/kastaneda/sandbox2020.de.co.ua/blob/master/bad-idea.php\n\n";
 echo "echo 'Hello world'\n";
 
