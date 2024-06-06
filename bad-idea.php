c673739da651bed8ec11f0ed7e8921eb0e16e8b1
Dmytro Kolesnykov <kastaneda@gmail.com>
2024-06-06 18:50:28 +0300

    LOL

--- bad-idea.php
+++ bad-idea.php
@@ -6,11 +6,12 @@ if (strpos($_SERVER['HTTP_USER_AGENT'], 'curl') === 0 ||
     exit;
 }
 
-//$url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-$url = 'https://sandbox2020.de.co.ua/bad-idea.php';
+$url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+// $url = 'http://sandbox2020.de.co.ua/bad-idea.php';
 
 header('Content-type: text/plain');
 echo "# How to run:\n# curl -sS $url | bash\n# wget $url -qO- | bash\n\n";
+echo "# Or this:\n# (U=\"$url\";[ -x /usr/bin/wget ]&&wget -qO- \$U||curl -sS \$U)|sh\n\n";
 echo "# Source: https://github.com/kastaneda/sandbox2020.de.co.ua/blob/master/bad-idea.php\n\n";
 echo "echo 'Hello world'\n";
 
